00d1d3535c67017686a180c5d39c60e6f5e5157f
Soso <so.business.2025@gmail.com>
2024-04-11 15:37:18 +0900

    演習問題2

---
+++ 20240411_Prac02_Hirayama.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class _20240411_Prac02_Hirayama
+    {
+        static void Main(string[] args)
+        {
+            // 変数宣言
+            int x= 7;
+
+            // 表示
+            Console.WriteLine(x * 3);
+            Console.WriteLine(x / 2);
+        }
+    }
+}
