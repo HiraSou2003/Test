60941678f4ac227dbf0eef3ffc8e118a6d10637c
student <student@407-01>
2024-04-11 14:18:05 +0900

    プロジェクト ファイルを追加します。

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console. WriteLine("あんたの名前は？");
+
+            string name = Console.ReadLine();                         // 入力
+            Console.WriteLine($"こんにちは、{name}さん");       // パターン１
+            //Console.WriteLine("こんにちは、" + name + "さん");  // パターン２
+        }
+    }
+}
