43b9388415dcb76530b8579d3ca63cc52aef3b3a
Scarlet20030225 <21010022hs@edu.fca.ac.jp>
2024-04-18 15:07:33 +0900

    変更

---
+++ 20240418_Prac1_Hirayama.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class _20240418_Prac1_Hirayama
+    {
+        static void Main(string[] args)
+        {
+            // 型推論
+            var x = 10;
+            var name = "John";
+
+            // 参照型
+            int[] numbers = new int[] { 1, 2, 3, };
+
+            // 結果表示
+            Console.WriteLine(x);
+            Console.WriteLine(name);
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            Console.WriteLine($"xの型は{x.GetType()}");
+            Console.WriteLine($"nameの型は{name.GetType()}");
+            Console.WriteLine($"numbersの型は{numbers.GetType()}");
+        }
+    }
+}
---
+++ 20240418_ex.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class _20240418_ex
+    {
+        // ポインタを使ったC#
+        static unsafe void PointerUsage()
+        {
+            int number = 10;
+
+            unsafe
+            {
+                int* ptr = &number;
+                Console.Write((int)ptr);
+            }
+        }
+
+        static void Main()
+        {
+            // 参照型
+            Console.WriteLine("参照型の例");
+            int[] ary1 = { 1, 2, 3 };
+            int[] ary2 = ary1;        // 参照をコピーするため、ary2もary1と同じオブジェクトとして参照
+
+            // ary2を変更
+            ary2[0] = 100;
+            Console.WriteLine($"ary1[0] = {ary1[0]}");
+
+            // 値型
+            Console.WriteLine("値型の例");
+            int x = 10;
+            int y = x;
+            x = 23;
+            Console.WriteLine($"x：{x}, y：{y}");
+        }
+    }
+}
